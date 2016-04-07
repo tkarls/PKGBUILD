caf9564e80db60a2d760164ba5530c2f6d1c94e1
M0Rf30 <morf3089@gmail.com>
2016-04-07 12:54:04 +0200

    update

--- openbazaard-git/openbazaard.sh
+++ openbazaard-git/openbazaard.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-cd /var/lib/openbazaard/
-export PYTHONPATH=/var/lib/openbazaard/:/usr/lib/python2.7/site-packages/
-ARGS="-t"
-python2 openbazaard.py "$@" $ARGS
+cd /var/lib/openbazaard
+python2 openbazaard.py start $ARGS
